1a2f1ca0f372e56d11530cbff10bb318222177db
6Morpheus6 <wanton-decay@gmx.net>
2025-04-10 08:12:05 +0200

    add

---
+++ torch.js
@@ -0,0 +1,98 @@
+module.exports = {
+  run: [
+    // nvidia 50 series
+    {
+      "when": "{{gpu === 'nvidia' && kernel.gpus && kernel.gpus.find(x => / 50.+/.test(x.model))}}",
+      "method": "shell.run",
+      "params": {
+        "venv": "{{args && args.venv ? args.venv : null}}",
+        "path": "{{args && args.path ? args.path : '.'}}",
+        "message": [
+          "uv pip install InvokeAI --python 3.12 --python-preference only-managed --index-url https://download.pytorch.org/whl/nightly/cu128 --reinstall",
+          "uv pip install -U bitsandbytes --no-deps"
+        ]
+      },
+      "next": null
+    },
+    // windows nvidia
+    {
+      "when": "{{platform === 'win32' && gpu === 'nvidia'}}",
+      "method": "shell.run",
+      "params": {
+        "venv": "{{args && args.venv ? args.venv : null}}",
+        "path": "{{args && args.path ? args.path : '.'}}",
+        "message": "uv pip install \"InvokeAI[xformers]\" --python 3.12 --python-preference only-managed --index=https://download.pytorch.org/whl/cu124 --reinstall"
+      }
+    },
+    // windows amd
+    {
+      "when": "{{platform === 'win32' && gpu === 'amd'}}",
+      "method": "shell.run",
+      "params": {
+        "venv": "{{args && args.venv ? args.venv : null}}",
+        "path": "{{args && args.path ? args.path : '.'}}",
+        "message": "uv pip install torch-directml torchaudio torchvision numpy==1.26.4"
+      }
+    },
+    // windows cpu
+    {
+      "when": "{{platform === 'win32' && (gpu !== 'nvidia' && gpu !== 'amd')}}",
+      "method": "shell.run",
+      "params": {
+        "venv": "{{args && args.venv ? args.venv : null}}",
+        "path": "{{args && args.path ? args.path : '.'}}",
+        "message": "uv pip install InvokeAI --python 3.12 --python-preference only-managed --index-url https://download.pytorch.org/whl/cpu --reinstall"
+      }
+    },
+    // apple mac
+    {
+      "when": "{{platform === 'darwin' && arch === 'arm64'}}",
+      "method": "shell.run",
+      "params": {
+        "venv": "{{args && args.venv ? args.venv : null}}",
+        "path": "{{args && args.path ? args.path : '.'}}",
+        "message": "uv pip install InvokeAI --python 3.12 --python-preference only-managed --index-url https://download.pytorch.org/whl/cpu --reinstall"
+      }
+    },
+    // intel mac
+    {
+      "when": "{{platform === 'darwin' && arch !== 'arm64'}}",
+      "method": "shell.run",
+      "params": {
+        "venv": "{{args && args.venv ? args.venv : null}}",
+        "path": "{{args && args.path ? args.path : '.'}}",
+        "message": "uv pip install InvokeAI --python 3.12 --python-preference only-managed --index-url https://download.pytorch.org/whl/cpu --reinstall"
+      }
+    },
+    // linux nvidia
+    {
+      "when": "{{platform === 'linux' && gpu === 'nvidia'}}",
+      "method": "shell.run",
+      "params": {
+        "venv": "{{args && args.venv ? args.venv : null}}",
+        "path": "{{args && args.path ? args.path : '.'}}",
+        "message": "uv pip install \"InvokeAI[xformers]\" --python 3.12 --python-preference only-managed --index=https://download.pytorch.org/whl/cu124 --reinstall"
+      }
+    },
+    // linux rocm (amd)
+    {
+      "when": "{{platform === 'linux' && gpu === 'amd'}}",
+      "method": "shell.run",
+      "params": {
+        "venv": "{{args && args.venv ? args.venv : null}}",
+        "path": "{{args && args.path ? args.path : '.'}}",
+        "message": "uv pip install InvokeAI --python 3.12 --python-preference only-managed --index-url https://download.pytorch.org/whl/rocm6.2.4 --reinstall"
+      }
+    },
+    // linux cpu
+    {
+      "when": "{{platform === 'linux' && (gpu !== 'amd' && gpu !=='nvidia')}}",
+      "method": "shell.run",
+      "params": {
+        "venv": "{{args && args.venv ? args.venv : null}}",
+        "path": "{{args && args.path ? args.path : '.'}}",
+        "message": "uv pip install InvokeAI --python 3.12 --python-preference only-managed --index-url https://download.pytorch.org/whl/cpu --reinstall"
+      }
+    }
+  ]
+}
