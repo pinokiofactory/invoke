93ed3115ca33e0bcd17f2294e6f3ab916b7a2627
6Morpheus6 <wanton-decay@gmx.net>
2025-11-06 07:39:44 +0100

    remove local sharing
    - remove proxy.start
    - remove comments

--- start.js
+++ start.js
@@ -1,42 +1,25 @@
 module.exports = {
   daemon: true,
   run: [
-    // Edit this step to customize your app's launch command
     {
       method: "shell.run",
       params: {
-        venv: "env",                // Edit this to customize the venv folder path
-        env: { },                   // Edit this to customize environment variables (see documentation)
+        venv: "env",
+        env: { },
         path: "app",
         message: [
           "invokeai-web",
         ],
         on: [{
-          // The regular expression pattern to monitor.
-          // When this pattern occurs in the shell terminal, the shell will return,
-          // and the script will go onto the next step.
-          "event": "/http:\/\/\\S+/",   
-
-          // "done": true will move to the next step while keeping the shell alive.
-          // "kill": true will move to the next step after killing the shell.
+          "event": "/http:\/\/\\S+/",
           "done": true
         }]
       }
     },
-    // This step sets the local variable 'url'.
-    // This local variable will be used in pinokio.js to display the "Open WebUI" tab when the value is set.
     {
       method: "local.set",
       params: {
-        // the input.event is the regular expression match object from the previous step
         url: "{{input.event[0]}}"
-      }
-    },
-    {
-      method: "proxy.start",
-      params: {
-        uri: "{{local.url}}",
-        name: "Local Sharing"
       }
     }
   ]
